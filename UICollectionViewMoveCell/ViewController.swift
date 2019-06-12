2f489e5aa4aae1cb649158280f4e4c7a0103ebd8
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-12 19:57:21 +0300

    Initial Commit

---
+++ UICollectionViewMoveCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewMoveCell
+//
+//  Created by Артем on 6/12/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
